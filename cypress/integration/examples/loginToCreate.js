9b27476718922ecd971dbe1abc6e1ebd371e5506
thirumagalkarthi <thirumagalkarthi@gmail.com>
2021-04-23 13:19:19 +0530

    First Commit

---
+++ cypress/integration/examples/loginToCreate.js
@@ -0,0 +1,11 @@
+describe('Login to Create',function(){
+
+    it('Login to Create with credentials',function(){
+        cy.visit('http://create-dev.vmgdigital.com/');
+        cy.title().should('eq', 'VMG Digital | Create Platform')
+        cy.get('[formcontrolname="username"]').type('tester6vmg@gmail.com');
+        cy.get('[formcontrolname="password"]').type('Test@123');
+        cy.get('#login').click();
+    })
+
+})
